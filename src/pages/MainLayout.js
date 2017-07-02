ee80b87135621743ee320d0166ce9b5ce0ab421b
Turian <aturian.2014@sis.smu.edu.sg>
2017-07-02 16:47:17 +0200

    Completed mobile xs view for main layout

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Icon, Row, Col } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 
 class MainLayout extends Component {
@@ -10,21 +10,38 @@ class MainLayout extends Component {
 
     render() {
         return (
-            <div>
+            <Layout>
                 <Layout>
-                    <Header id="layout-header">
-                        <div className="logo" />
-                        <Menu mode="horizontal" style={{ lineHeight: '64px', float: 'right' }}>
-                            <Menu.Item key="1">APPLY</Menu.Item>
-                            <Menu.Item key="2">COLLECTIONS</Menu.Item>
-                            <Menu.Item key="3">FAQ</Menu.Item>
-                            <Menu.Item key="4">CONCIERGE</Menu.Item>
-                        </Menu>
+                    <Header id="layout-header" >
+                        <Row type="flex" justify="space-between">
+                            <Col xs={24} md={6}>
+                                <div className="logo" />
+                            </Col>
+                            <Col xs={24} md={7} >
+                                <Menu mode="horizontal" style={{ lineHeight: '64px' }}>
+                                    <Menu.Item key="1">APPLY</Menu.Item>
+                                    <Menu.Item key="2">COLLECTIONS</Menu.Item>
+                                    <Menu.Item key="3">FAQ</Menu.Item>
+                                    <Menu.Item key="4">CONCIERGE</Menu.Item>
+                                </Menu>
+                            </Col>
+                        </Row>
                     </Header>
-                    <Content style={{ padding: '0 2em' }}>Content</Content>
-                    <Footer>Footer</Footer>
+                    <Content style={{ padding: '0 2em' }}>
+                        CONTENT 123
+                        {this.props.children}
+                    </Content>
+                    <Footer>
+                        <Row type="flex" justify="space-between">
+                            <Col xs={24} md={4}>CONTACT OUR CONCIERGE</Col>
+                            <Col xs={24} md={5}>
+                                <span style={{ marginRight: '2em' }}><Icon type="mail" /> email@email.com</span>
+                                <Icon type="phone" /> +65-1234-1234
+                            </Col>
+                        </Row>
+                    </Footer>
                 </Layout>
-            </div>
+            </Layout>
         );
     }
 }
