1f84df022217a7e76f8a6104219a697a2eba97ce
Tananphu@visa.com <tananphu@visa.com>
2017-07-25 15:05:28 +0800

    Integrated with mailchimp

--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col, Form, Input, Button } from 'antd';
 import { Redirect } from 'react-router-dom';
+import SubscribeFrom from 'react-mailchimp-subscribe'
 
 class ComingSoon extends Component {
 
@@ -12,10 +13,10 @@ class ComingSoon extends Component {
     render() {
         return (
             <div>
-                <Row type="flex" justify="center" align="middle" style={{ height: '100vh', textAlign: 'center', backgroundPosition:'center', backgroundImage: 'url("img/comingsoon-bg.jpg")' }} >
+                <Row type="flex" justify="center" align="middle" style={{ height: '100vh', textAlign: 'center', backgroundPosition: 'center', backgroundImage: 'url("img/comingsoon-bg.jpg")' }} >
                     <Col xs={20} >
-                        <h1 style={{fontSize: '3em'}}>SPECTER ONE</h1>
-                        <br/><br/><br/><br/><br/><br/>
+                        <h1 style={{ fontSize: '3em' }}>SPECTER ONE</h1>
+                        <br /><br /><br /><br /><br /><br />
                         <h2>COMING SOON...</h2>
                         <br /><br />
                         <h1>THE ADVENTURE BEGINS</h1>
@@ -40,7 +41,6 @@ class OptInForm extends Component {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                // document.location.href = "mailto:lin@specterone.com&body=" + values;
                 console.log('Received values of form: ', values);
             }
         });
@@ -52,28 +52,31 @@ class OptInForm extends Component {
 
     render() {
         const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
-
+        const formProps = {
+            action: '//specterone.us15.list-manage.com/subscribe/post?u=3b5936bf6be301a299947c2f3&amp;id=1344a8f363',
+            messages: {
+                inputPlaceholder: "E-mail",
+                btnLabel: "NOTIFY ME",
+                sending: "Subscribing",
+                success: "Please check your email to confirm subscription",
+                error: "You can't subscribe with this email"
+            },
+            styles: {
+                sending: {
+                    color: "auto"
+                },
+                success: {
+                    color: "white"
+                },
+                error: {
+                    color: "red"
+                }
+            }
+        }
         return (
-            <Form layout="inline" onSubmit={this.handleSubmit}>
-                <Form.Item  style={{marginTop: '1.5em'}}>
-                    {getFieldDecorator('email', {
-                        rules: [{
-                            type: 'email', message: 'The E-mail is invalid',
-                        }, {
-                            required: true, message: 'Please enter your E-mail',
-                        }],
-                    })(
-                        <Input placeholder="E-mail" style={{width: '250px'}} />
-                    )}
-                </Form.Item>
-                <Form.Item style={{marginTop: '1.5em'}}>
-                    <Button style={{width: '150px'}}
-                        htmlType="submit"
-                        disabled={this.hasErrors(getFieldsError())}>
-                        NOTIFY ME
-                                </Button>
-                </Form.Item>
-            </Form>
+            <div>
+                <SubscribeFrom {...formProps} className='optin-form' />
+            </div>
         );
     }
 }
