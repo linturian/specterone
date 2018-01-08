6c6bcd77da3400da0fbd4348536a07ed1412f08f
unknown <linturian@gmail.com>
2018-01-09 02:07:12 +0800

    Overlay coming soon

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -13,15 +13,20 @@ class Collection extends Component {
 
     render() {
         return (
-            <section>
-                <Row className="dark-theme">
-                    <Col xs={24} style={{ textAlign: 'center', padding: '0 0 1.5em 0' }}>
-                        <h1 style={{ fontWeight: 'bolder' }}>COLLECTION</h1>
-                    </Col>
-                </Row>
-                <TierCollection watchList={enthusiastCollection} tierName="ENTHUSIAST" />
-                <TierCollection watchList={aficionadoCollection} tierName="AFICIONADO" />
-            </section>
+            <div>
+                <div style={{ backgroundColor: 'rgba(0,0,0,0.8)', zIndex: '1', position: 'fixed', height: '-webkit-fill-available', width: '-webkit-fill-available' }}>
+                    <h1 style={{ textAlign: 'center', lineHeight: '90vh', color: 'white', fontWeight: 'bolder', fontSize: '3em'}}>COMING SOON</h1>
+                </div>
+                <section>
+                    <Row className="dark-theme">
+                        <Col xs={24} style={{ textAlign: 'center', padding: '0 0 1.5em 0' }}>
+                            <h1 style={{ fontWeight: 'bolder' }}>COLLECTION</h1>
+                        </Col>
+                    </Row>
+                    <TierCollection watchList={enthusiastCollection} tierName="ENTHUSIAST" />
+                    <TierCollection watchList={aficionadoCollection} tierName="AFICIONADO" />
+                </section>
+            </div>
         )
     }
 }
